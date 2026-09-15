3e7db92862acac2c7e118c0d1a9ae0034025245b
Marcelandoo <marcelo.henrico@ifsuldeminas.edu.br>
2026-09-15 13:30:31 -0300

    Finalizou a parte com o Mateus

--- app/src/main/java/com/example/projetopdmii/tela03.java
+++ app/src/main/java/com/example/projetopdmii/tela03.java
@@ -1,38 +1,40 @@
 package com.example.projetopdmii;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
-public class MainActivity extends AppCompatActivity implements Runnable{
+import java.util.ArrayList;
 
-    private Button btn;
-    private Handler handler;
+public class tela03 extends AppCompatActivity {
+
+    private ViewPager2 viewpager;
+    private ArrayList<Slide> lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tela03);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        handler = new Handler();
-        handler.postDelayed(this, 2000);
-    }
-
-    @Override
-    public void run() {
-        startActivity(new Intent(this, tela03.class));
+        viewpager = findViewById(R.id.viewpager);
+        lista = new ArrayList<Slide>();
+        lista.add(new Slide("Slide 1", R.drawable.jeff, "text"));
+        lista.add(new Slide("Slide 2", R.drawable.jim, "text"));
+        lista.add(new Slide("Slide 3", R.drawable.kate, "text"));
+        lista.add(new Slide("Slide 4", R.drawable.rosalia, "text"));
+        lista.add(new Slide("Slide 5", R.drawable.sufjan, "text"));
+        SlideAdapter adapter = new SlideAdapter(lista);
+        viewpager.setAdapter(adapter);
     }
 }
